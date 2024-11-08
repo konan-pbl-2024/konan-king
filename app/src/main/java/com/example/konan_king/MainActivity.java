8f4ed8b3ebe1ec77d4bcad87a75671deaafdd712
kaimochihi <m.ma134728@icloud.com>
2024-11-08 16:38:02 +0900

    画面切り替え完了（タイトルから問題画面）

--- app/src/main/java/com/example/konan_king/MainActivity.java
+++ app/src/main/java/com/example/konan_king/MainActivity.java
@@ -3,7 +3,9 @@ package com.example.konan_king;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,6 +15,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button start_button = (Button)findViewById(R.id.start_button);
+            start_button.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Intent intent = new
+                            Intent(MainActivity.this, MonadaiActivity.class);
+                    startActivity(intent);
+                }
+            });
 
 
         progressDialog = new ProgressDialog(this);
@@ -23,6 +34,8 @@ public class MainActivity extends AppCompatActivity {
 
         AsyncTaskClass task = new AsyncTaskClass(progressDialog);
         task.execute("");
+
+
 
     }
 }
