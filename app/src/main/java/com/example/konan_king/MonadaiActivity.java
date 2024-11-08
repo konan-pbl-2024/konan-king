472ccf6c7830b074a13ff25360c8cd383d90f51a
Ichi-Kisogawa <daikouyuuta@gmail.com>
2024-11-08 14:13:03 +0900

    monadai作った

--- app/src/main/java/com/example/konan_king/MonadaiActivity.java
+++ app/src/main/java/com/example/konan_king/MonadaiActivity.java
@@ -5,12 +5,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MonadaiActivity extends AppCompatActivity{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.mondai);
         Button okButton = (Button)findViewById(R.id.ans1);
     }
+
 }
