cace67c5c2cec7eb0c3b9aab93bdd428b36fa197
kaimochihi <m.ma134728@icloud.com>
2024-11-08 17:24:29 +0900

    画面切り替え完了（タイトルから問題画面）

--- app/src/main/java/com/example/konan_king/MonadaiActivity.java
+++ app/src/main/java/com/example/konan_king/MonadaiActivity.java
@@ -2,15 +2,31 @@ package com.example.konan_king;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.widget.Button;
 
 public class MonadaiActivity extends AppCompatActivity{
+
+    ProgressDialog progressDialog;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.mondai);
         Button okButton = (Button)findViewById(R.id.ans1);
+
+        CharByCharTextView textView = new CharByCharTextView(this);
+        textView.setTargetText("テスト用の文字列です。");
+
+
+        progressDialog = new ProgressDialog(this);
+        progressDialog.setTitle("");
+        progressDialog.setMessage("ロード中...");
+        progressDialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+        progressDialog.show();
+
+        AsyncTaskClass task = new AsyncTaskClass(progressDialog);
+        task.execute("");
     }
 
 }
