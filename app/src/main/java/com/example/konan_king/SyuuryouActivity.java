8f45d224448f0fd559e3bcf3e46ecffacd38d755
kaimochihi <m.ma134728@icloud.com>
2024-11-15 13:57:26 +0900

    終了画面からタイトル画面に戻る

--- app/src/main/java/com/example/konan_king/SyuuryouActivity.java
+++ app/src/main/java/com/example/konan_king/SyuuryouActivity.java
@@ -2,7 +2,11 @@ package com.example.konan_king;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 
 public class SyuuryouActivity extends AppCompatActivity {
 
@@ -10,5 +14,15 @@ public class SyuuryouActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_syuuryou);
+
+        Button button = findViewById(R.id.Title);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // ActivityBへ遷移
+                startActivity(new Intent(SyuuryouActivity.this, MainActivity.class));
+
+            }
+        });
     }
 }
